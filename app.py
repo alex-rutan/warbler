a5140c9e2b5aae95ea11a42c7caf26d41f9c898c
ZeeTom <zthom13@hotmail.com>
2021-07-22 12:31:39 -0600

    added user likes route and template. Added condition to view user warbles when not logged in, but no additional access authorized.

--- app.py
+++ app.py
@@ -187,6 +187,20 @@ def users_followers(user_id):
     user = User.query.get_or_404(user_id)
     return render_template('users/followers.html', user=user)
 
+@app.route('/users/<int:user_id>/likes')
+def users_likes(user_id):
+    """Show list of user's likes"""
+
+    if not g.user:
+        flash("Access unauthorized.", "danger")
+        return redirect("/")
+
+    user = User.query.get_or_404(user_id)
+    return render_template('users/likes.html', user=user)
+
+    
+
+
 
 @app.route('/users/follow/<int:follow_id>', methods=['POST'])
 def add_follow(follow_id):
